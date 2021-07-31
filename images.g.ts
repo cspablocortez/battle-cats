37840b8e0074399f8484d7ac62727439b0dda026
cspablocortez <57056367+cspablocortez@users.noreply.github.com>
2021-07-31 12:25:51 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,141 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "myAnim":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+            case "myAnim0":
+            case "anim2":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . c c c c . . . . 
+. . . . . . c c 1 1 1 1 c c . . 
+. . . . . c 1 1 1 1 1 1 1 1 c . 
+. . . . c 1 1 1 f 1 1 1 1 1 1 c 
+. . . c 1 1 1 1 f f 1 1 1 1 1 c 
+. . . c 1 1 1 1 1 1 1 1 1 1 1 c 
+. . c d 1 1 1 1 1 1 b 1 b b c c 
+. . c d d d 1 1 1 1 1 3 3 3 1 c 
+. . c d d d 1 1 1 1 1 1 1 1 b . 
+. c c d d d d b 1 1 c b b c . . 
+c d c d d d d b b 1 1 c b b c . 
+c d d d d d d d d c c c c c c . 
+. c d d d b 1 1 d c c c c . . . 
+. . c c c b 1 1 b c c c c c . . 
+. . . . c b 1 1 d c b b b c . . 
+`, img`
+. . . . . . . c c c c c . . . . 
+. . . . . . c 1 1 1 1 1 c c . . 
+. . . . . c 1 1 f 1 1 1 1 1 c . 
+. . . . c 1 1 1 f f 1 1 1 1 1 c 
+. . . c 1 1 1 1 1 1 1 1 1 1 1 c 
+. . . c 1 1 1 1 1 1 b 1 b b c c 
+. . c d 1 1 1 1 1 1 1 3 3 3 1 c 
+. . c d d d 1 1 1 1 1 3 3 3 1 c 
+. . c d d d 1 1 1 b 1 1 1 1 c . 
+. . c d d d d b 1 1 c b b c . . 
+c c c d d d d b b 1 1 c b b c . 
+c d d d d d d d d c c c c c c . 
+. c d d d b 1 1 b c c c . . . . 
+. . c c c b b 1 1 d c . . . . . 
+. . . . . c c c c c c c . . . . 
+. . . . . . . c b b b c . . . . 
+`, img`
+. . . . . . . c c c c c . . . . 
+. . . . . . c 1 1 1 1 1 c c . . 
+. . . . . c 1 1 f 1 1 1 1 1 c . 
+. . . . c 1 1 1 f f 1 1 1 1 1 c 
+. . . c 1 1 1 1 1 1 1 1 1 1 1 c 
+. . . c 1 1 1 1 1 1 b 1 b b c c 
+. . c d 1 1 1 1 1 1 1 3 3 3 1 c 
+. . c d d d 1 1 1 1 1 1 1 1 1 c 
+. . c d d d 1 1 1 b 1 1 1 1 c . 
+. . c d d d d b 1 1 c b b c . . 
+c c c d d d d b b 1 1 c b b c . 
+c d d d d d d d d c c c c c c . 
+. c c d d d b 1 1 b c c . . . . 
+. . . c c c b b 1 1 d c . . . . 
+. . . . . c c c c c c c . . . . 
+. . . . . . . c b b b c . . . . 
+`, img`
+. . . . . . . c c c c c . . . . 
+. . . . . . c 1 1 1 1 1 c c . . 
+. . . . . c 1 1 f 1 1 1 1 1 c . 
+. . . . c 1 1 1 f f 1 1 1 1 1 c 
+. . . c 1 1 1 1 1 1 1 1 1 1 1 c 
+. . . c 1 1 1 1 1 1 b 1 b b c c 
+. . c d 1 1 1 1 1 1 1 3 3 3 1 c 
+. . c d d d 1 1 1 1 1 1 1 1 1 c 
+. . c d d d 1 1 1 b 1 1 1 1 c . 
+. . c d d d d b 1 1 c b b c . . 
+. c c d d d d b b 1 1 c b b c . 
+c c d d d d d d b b c c c c c . 
+c d d d d d 1 1 b 1 1 c c . . . 
+c c c c c c b b 1 1 b c . . . . 
+. . . . . . c c c c c c . . . . 
+. . . . . . c b b b c . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . c c c c . . . . 
+. . . . . . c c 1 1 1 1 c c . . 
+. . . . . c 1 1 1 1 1 1 1 1 c . 
+. . . . c 1 1 1 f 1 1 1 1 1 1 c 
+. . . c 1 1 1 1 f f 1 1 1 1 1 c 
+. . . c 1 1 1 1 1 1 1 1 1 1 1 c 
+. . c d 1 1 1 1 1 1 b 1 b b c c 
+. . c d d d 1 1 1 1 1 3 3 3 1 c 
+. . c d d d 1 1 1 1 1 1 1 1 b . 
+. . c d d d d b 1 1 c b b c . . 
+c c c d d d d b b 1 1 c b b c . 
+c d d d d d d d d c c c c c c . 
+. c c d d b 1 1 d c c c c . . . 
+. . . c c b 1 1 c c c b b c . . 
+. . . . . c 1 1 d c c c c c . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . c c c c . . . . 
+. . . . . . c c 1 1 1 1 c c . . 
+. . . . . c 1 1 1 1 1 1 1 1 c . 
+. . . . c 1 1 1 f 1 1 1 1 1 1 c 
+. . . c 1 1 1 1 f f 1 1 1 1 1 c 
+. . . c 1 1 1 1 1 1 b 1 b b c c 
+. . c d 1 1 1 1 1 1 b b 3 3 c c 
+. . c d d d 1 1 1 1 1 3 3 3 1 c 
+. . c d d d 1 1 1 1 1 1 1 1 b . 
+. . c d d d d b 1 1 c b b c . . 
+c c c d d d d b b 1 1 c b b c . 
+c d d d d d d d d c c c c c c . 
+c c d d d b 1 1 d c c c c . . . 
+. . c c c b 1 1 b c c b c . . . 
+. . . . . c b 1 1 d c c c . . . 
+`];
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
